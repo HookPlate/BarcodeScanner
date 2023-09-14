dc95fa2e7c01119623ba4188847432496775dd52
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-09-14 15:11:57 +0100

    Adds the connection between BarcodeScannerView and ScannerView

--- BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -8,20 +8,23 @@
 import SwiftUI
 
 struct BarcodeScannerView: View {
+    
+    @State private var scannedCode = ""
+    
     var body: some View {
         NavigationView {
             VStack {
-                ScannerView()
+                ScannerView(scannedCode: $scannedCode)
                     .frame(maxWidth: .infinity, maxHeight: 300)
                 Spacer()
                     .frame(height: 60)
                 //a label is a little icon, then words
                 Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
                     .font(.title)
-                Text("Not Yet Scanned")
+                Text(scannedCode.isEmpty ?  "Not Yet Scanned" : scannedCode)
                     .bold()
                     .font(.largeTitle)
-                    .foregroundColor(.green)
+                    .foregroundColor(scannedCode.isEmpty ? .red : .green)
                     .padding()
                 
             }
